10aa0340f18ac96edb73adea356532bcc1906831
Tim Aerts <aardbeiplantje@gmail.com>
2025-01-31 22:54:30 +0100

    ai.sh: cli tool for cerebras AI service using curl, jq and bash

---
+++ ai.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/bash
+
+
+if [ -f ~/.airc ]; then
+    source ~/.airc
+fi
+if [ -z "$CEREBRAS_API_KEY" ]; then
+    echo "Please set CEREBRAS_API_KEY environment variable"
+    exit 1
+fi
+hs=~/.ai_history
+hp=~/.ai_prompt
+if [ ! -s $hs ]; then
+    echo '{"role":"system","content":""}' > $hs
+fi
+
+function crbrs_chat_completion(){
+    s=$*
+    s=$(echo -n "$s"|jq -RsaMj .)
+    echo '{"role":"user","content":'$s'}' >>$hs
+    jstr=$(<$hs)
+    jstr=${jstr//$'\n'/,}
+    [ "$DEBUG" == 1 ] && echo "[$jstr]"|jq -r >/dev/stderr
+    fn=/tmp/ai.$USER.$$
+    curl -qsSkL 'https://api.cerebras.ai/v1/chat/completions' \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer ${CEREBRAS_API_KEY}" \
+    --data '{
+      "model": "llama-3.3-70b",
+      "max_tokens": 8192,
+      "stream": false,
+      "messages": ['"${jstr}"'],
+      "temperature": 0,
+      "top_p": 1
+    }' > $fn
+    [ "$DEBUG" == 1 ] && jq -r >/dev/stderr <$fn
+    jq -r '.choices[0].message.content' <$fn
+    resp=$(jq -ra '.choices[0].message.content' <$fn)
+    rm -f $fn
+    echo '{"role":"assistant","content":'$resp'}' >>$hs
+}
+
+function crbrs_chat(){
+    history -r $hp
+    set -o vi
+    while true; do
+        read -r -e -p "|vi|> " what
+        if [ "$what" == "" ]; then
+            continue
+        fi
+        if [[ "$what" =~ ^/system[[:blank:]]+(.*)$ ]]; then
+            echo '{"role":"system","content":"'${BASH_REMATCH[1]}'"}' > $hs
+            continue
+        fi
+        if [ "$what" == "/exit" -o "$what" == "/quit" ]; then
+            break
+        fi
+        if [ "$what" == "/clear" ]; then
+            echo '{"role":"system","content":""}' > $hs
+            continue
+        fi
+        if [ "$what" == "/history" ]; then
+            cat $hs
+            continue
+        fi
+        if [ "$what" == "/debug" ]; then
+            DEBUG=1
+            continue
+        fi
+        if [ "$what" == "/nodebug" ]; then
+            DEBUG=0
+            continue
+        fi
+        if [ "$what" == "/help" ]; then
+            echo "/exit, /quit, /clear, /history, /help, /debug, /nodebug, /system"
+            continue
+        fi
+        history -s "$what"
+        crbrs_chat_completion "$what"
+    done
+    history -w $hp
+}
+
+crbrs_chat
